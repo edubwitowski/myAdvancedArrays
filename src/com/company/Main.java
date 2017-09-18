f82c318a85b01e8e66eb6a03a782198808e2c7f8
edubwitowski <31215451+edubwitowski@users.noreply.github.com>
2017-09-17 21:54:36 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+
+
+
+        public class myDogArray {
+
+            public static void main(String[] args) {
+
+
+                String[] myDogArray;
+
+                myDogArray = new String[4];
+
+
+                myDogArray[0] = "Ketchup";
+                System.out.println(myDogArray[0]);
+
+
+
+                myDogArray[1] = "mustard";
+                System.out.println(myDogArray[1]);
+
+
+                myDogArray[2] = "Relish";
+                System.out.println(myDogArray[2]);
+
+
+                myDogArray[3] = "Mayo";
+                System.out.println(myDogArray[3]);
+
+
+                myDogArray[4] = "Onion";
+                System.out.println(myDogArray[4]);
+            }
+        }
+
+
